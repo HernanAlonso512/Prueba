2f174c9be44bebdd7d6db96f76007dbddfe04ac3
HernanAlonso <hernanalonso512@gmail.com>
2020-11-25 16:12:50 +0100

    trolleando

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Antoñito, antoñito mágico, ¿que lenguaje es el más guapo?");
+        }
+    }
+}
